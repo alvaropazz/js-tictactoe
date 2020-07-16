38e7ad1ed69576a759b028e690b9b10af7e3514c
JayKowski <jaymaina5@gmail.org>
2020-07-16 18:39:07 +0300

    added some logic

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-
 let gameBoard = ['','','','','','','','','','']
 
-function player(name, token){
-   this.name,
-   this.token
+function player (name, token){
+      this.name = name,
+      this.token = token
 }
 
+
+let player1 = new player('Kowski', "T");
+let player2 = new player('kim', 'x');
+
+function askNameAndToken(){
+ 
+}
+// console.log(player1.name, player1.token, player2.name, player2.token)
+
+askNameAndToken()
