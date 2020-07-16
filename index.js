671ead03cec286673fac360de60ec6baea55746d
JayKowski <jaymaina5@gmail.org>
2020-07-16 20:15:26 +0300

    added wins check and gameBoard update

--- index.js
+++ index.js
@@ -1,17 +1,109 @@
-let gameBoard = ['','','','','','','','','','']
+let gameBoard = ['','','','','','','','','']
+
+
+// required funtions
+// 1. get player details
+// 2. player turn
+// 3. Check wins/tiesk
+// 4. update game board
+// 5. Game over
+// 6. GAME FLOW
+// 7. RESET GAME
 
 function player (name, token){
       this.name = name,
       this.token = token
 }
 
+const player1 = "X"
+const player2 = "O"
 
-let player1 = new player('Kowski', "T");
-let player2 = new player('kim', 'x');
-
-function askNameAndToken(){
- 
-}
 // console.log(player1.name, player1.token, player2.name, player2.token)
 
-askNameAndToken()
+// askNameAndToken()
+
+let movesLeft = () => {
+      const moves = 9
+      function checkMoves(){
+            return moves
+      }
+      function deductMoves(){
+            moves -= 1
+            return moves
+      }
+      return { checkMoves() 
+            ,deductMoves()
+      }
+}
+
+function playerTurn(){
+      const play = document.querySelector('.game-board');
+      play.addEventListener('click', function(e){
+            const cell = e.target
+            let turn = 1
+            let player;
+            if (turn % 2 !== 0) {
+                  player = player1
+                  turn += 1
+            } else {
+                  player = player2
+                  turn += 1
+            }
+            return player;
+      })
+}
+      // let odd = turn
+      // let even = turn
+
+
+const boardOutput = document.querySelector('.game-board')
+
+boardOutput.addEventListener('click', function(e){
+      const cell = e.target
+      const tokenChange = e.target.innerText = "x"
+      gameBoard[cell.getAttribute('data-index')] = tokenChange
+      // console.log(gameBoard)
+      // wins(gameBoard)
+      // console.log(cell.getAttribute('data-index'), tokenChange)
+})
+
+function wins(board){
+      if(board[0] && board[1] && board[2] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[0] && board[3] && board[6] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[0] && board[4] && board[8] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[3] && board[4] && board[5] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[6] && board[7] && board[8] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[1] && board[4] && board[7] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[2] && board[5] && board[8] === 'x'){
+            console.log("X wins the Game!!!")
+      } else if (board[2] && board[4] && board[6] === 'x'){
+            console.log("X wins the Game!!!")
+      } else {
+            console.log("Its a tie :-( ")
+      }
+}
+///////////////// STORING PLAYERS //////////////////////
+// function storePlayers(){
+//       const player1 = "X"
+//       const player2 = "O"
+
+//       function playerOne(){
+//             return player1
+//       }
+
+//       function playerTwo(){
+//             return player2
+//       }
+
+//       return { playerOne()
+//             , playerTwo() }
+// }
+
+// let player1 = new player('Kowski', "T");
+// let player2 = new player('kim', 'x');
