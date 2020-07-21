65de3b0cc6b4d6689b757c0be2eee555cee267d9
A L V A R O <alvarop3000@gmail.com>
2020-07-21 11:41:32 -0500

    fix linter

--- gameboardreset.js
+++ gameboardreset.js
@@ -1,5 +1,6 @@
 const gameBoardReset = () => {
   const squares = document.querySelectorAll('.game-board .cell');
+  // eslint-disable-next-line no-return-assign
   Array.from(squares).forEach((e) => (e.innerText = ''));
 };
 
